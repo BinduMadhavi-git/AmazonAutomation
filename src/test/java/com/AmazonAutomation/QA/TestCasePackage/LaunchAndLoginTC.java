16929bec3a92d5332c16e5d81bd238ea357f65f2
Bindu <ybm.madhavi@gmail.com>
2023-02-15 16:33:44 +0530

    This is newly Changed Changesto the LaunchAndLogin

--- src/test/java/com/AmazonAutomation/QA/TestCasePackage/LaunchAndLoginTC.java
+++ src/test/java/com/AmazonAutomation/QA/TestCasePackage/LaunchAndLoginTC.java
@@ -39,6 +39,6 @@ public class LaunchAndLoginTC	extends BaseClass
   
   public void display()
   {
-	  System.out.println("Hello This is the first change");
+	  System.out.println("Hello This is the second change");
   }
 }
